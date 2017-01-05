61242910e26f41da01b27ad653d918c8c63069e0
rylopez <yohanny_116@hotmail.com>
2017-01-04 22:42:10 -0500

    04 enero

--- view/nuevo_producto.php
+++ view/nuevo_producto.php
@@ -1,110 +1,124 @@
- <?php 
-  
-  require_once("../model/db_conn.php");
 
-    require_once("../model/proveedores.class.php");
+ <div class="row contenedor">
+
+<div class="col-sm-12 col-md-7 col-lg-9 formulario">
  
-  $proveedor=Gestion_proveedores::ReadAll();
-?>
-<?php
+<div class="form-style-6">
 
-          if(isset($_GET["m"])){
-            if($_GET["m"] != ""){
-              echo "<script>alert('".$_GET["m"]."')</script>";
-            }
-          }
+ 
+  <form  id action="../controller/productos.controller.php" method="POST" enctype="multipart/form-data">
+        <h3 >Nuevo Producto</h3>
+        
+        
 
+          <input   type="text" placeholder="Referencia" name="Referencia"  required  />
+              
+            <input  type="text" placeholder="Nombre" name="nombre"  required />             
+            
+            <input   type="number" name="valor_compra" placeholder="Valor Compra" required />
+
+            <input   type="number" name="valor_venta" placeholder="Valor Venta" required />
+
+            <input   type="number" name="descuento" placeholder="Descuento" required />
+
+            <input   type="number" name="iva" placeholder="Iva" required />
+              http://casamadrugada.net/tutoriales/php/como-subir-archivos-imagenes-utilizando-php-y-mysql/
+            
+            <input value="<?php echo $usuario[8] ?>"  type="number" name="celular" placeholder="Número Celular"  required size="11" />
+          
+            <input value="<?php echo $usuario[7] ?>"  type="number" name="telefono"  placeholder="Número telefofico" required size="10" />
+          
+            
+            <input value="<?php echo $usuario[9] ?>"  type="text" name="direccion"  placeholder="Dirección" required/ >
+         
+           
+            <input value="<?php echo $usuario[10] ?>"  type="text" name="ciudad" placeholder="Ciudad de residencia" required / >
+            
+          
+            
+            <input value="<?php echo $usuario[5] ?>"  type="email" name="correo" placeholder="Correo electronico"  required/ >
+           
+            
+            <input value="<?php echo $usuario[6] ?>"  type="password" name="clave1"  placeholder="Ingrese Contraseña" required/>
+            
+           
+            <input value="<?php echo $usuario[6] ?>"  type="password" name="clave2" placeholder="Repita Contraseña" required/>
+            
+          
+      
+         
+            <input value="<?php echo $usuario[11] ?>"  type="number" name="edad" placeholder="Edad" required/ >
+           
+         
+           <select name="sexo"required >
+            <option value="x" disabled selected>Seleccione Genero</option>
+            <option value="Femenino"<?php if($usuario[12] == "Femenino"){ echo "selected"; } ?> >Femenino</option>
+            <option value="Masculino" <?php if($usuario[12] == "Masculino"){ echo "selected"; } ?>>Masculino</option>
+            <option value="otro" <?php if($usuario[12] == "otro"){ echo "selected"; } ?>>otro</option>
+          </select>
+            
+        <?php
+
+
+    
+        if(!isset($_SESSION["id_usuario"])){ ?>
+        <input name="id_rol" value="<?php echo $usuario[13] ?>"  type="hidden" />
+          
+        <?php    }else{ 
+          if ($_SESSION["id_rol"]==1) {?>
+        
+                <select    name="id_rol" placeholder="Rol Usuario" required>
+                    <option value="x" disabled selected>Seleccione el Rol</option>
+                    <option value="1" <?php if($usuario[13] == "1"){ echo "selected"; } ?>>Administrador</option>
+                    <option value="2" <?php if($usuario[13] == "2"){ echo "selected"; } ?>>Empleado</option>
+                    <option value="3" <?php if($usuario[13] == "3"){ echo "selected"; } ?>>Cliente</option>                    
+                </select>
+         
+        <?php }else{  ?>
+           
+                <select    name="id_rol" required placeholder="Rol Usuario">
+                    <option value="" disabled selected>Seleccione el Rol</option>
+                    <option value="2" <?php if($usuario[13] == "2"){ echo "selected"; } ?>>Empleado</option>
+                    <option value="3" <?php if($usuario[13] == "3"){ echo "selected"; } ?>>Cliente</option>                    
+                </select>
+            
+      <?php } } ?>
+      
+      
+      
+      <input type="hidden" name="estado" value="1">
+      <input type="hidden" name="id_usuario" value="<?php echo $usuario[0] ?>" >
+      <input type="hidden" name="autor" value="<?php echo ($_SESSION["nombre"])." ".($_SESSION["apellido"]); ?>">
+
+
+
+      <button class="guardar"   type="botton" name="acc" value="u">Guardar</button>
+            <?php
+
+      if(!isset($_SESSION["id_usuario"])){
+                 
       ?>
- <div class="container s12 m10 l9" id="form">
+      <button type="botton" class="cancelar" href="index.php">Cancelar</button>   
+      <?php 
+       }else {
+         if ($_SESSION["id_rol"]==1 ) {  
+      ?>
+      
+      <button   type="botton" class="cancelar" href="index.php?p=<?php echo base64_encode("gestion_usuarios"); ?>">Cancelar</button >
+      <?php 
+       }}
+      ?>
+            
 
-    <form id="registrate" class="col s12 " action="../controller/productos.controller.php" method="POST">
-        <h2 class="center">Nuevo Producto</h2>
+     
 
-        <div class="row" >
-          <div class="col s12">
-                <div class="input-field col s12 m6 black-text" >
-                <label class="black-text">Referencia Producto</label>
-                <br>
-                <input type="text" name="referencia" class="validate" required />
-              </div>
-                <div class="input-field col s12 m6 black-text ">
-                  <label class="black-text">Nombre Producto</label>
-                  <br>
-                  <input type="text" name="nombre" class="validate" required onkeypress="return validar(event)"  />
-              </div>
-        </div> 
-        </div>
-          <div class="row" >
-          <div class="col s12">
-              <div class="input-field col s12 m6 black-text" >
-                <label class="black-text">Valor Compra</label>
-                <br>
-                <input type="number" name="valor_compra" class="validate" required />
-              </div>
-              <div class="input-field col s12 m6 black-text">
-                <label class="black-text">Valor Venta</label>
-                <br>
-                <input type="number" name="valor_venta" class="validate" required />
-              </div>
-            </div>
-          </div>
-      <div class="row" >
-          <div class="input-field col s12 m6 black-text" >
-            <label class="black-text">Valor Iva</label>
-            <br>
-            <input type="number" name="iva" class="validate" required size="11" />
-          </div> 
-
-          <div class="input-field col s12 m6 black-text">
-            <label class="black-text">Porcentaje Descuento</label>
-            <br>
-            <input type="number" name="descuento" class="validate"  size="10" />
-          </div>
-      </div>
-      <div class="row" >
-        <div class="col s12">
-          <div class="input-field col s12 m6 black-text">
-            <label class="black-text">Cantidad Existencias</label>
-            <br>
-            <input type="number" name="cant_existente" class="validate" required/ >
-          </div>
-
-          <div class="input-field col s12 m6 black-text">
-            <label class="black-text">Tipo de Productos</label>
-              <br>
-              <select name="id_tipoproducto" required >
-                <option value="" disabled selected>Seleccione</option>
-                <option value="1" >Insumos</option>
-                <option value="2" >Cosmeticos</option>
-                <option value="3" >Quimicos</option>
-              </select>          
-          </div>
-        </div>
-      </div>
-      <div class="row" >
-        <div class="col s12">
-          <div class="input-field col s12 m6 black-text">
-            <label class="black-text">Seleccione Proveedor</label>
-              <br>
-              <select name="id_proveedor" required >
-                <option value="" disabled selected>Seleccione</option>
-                <?php 
-
-                foreach ($proveedor as $row) {
-        echo '<option value="'.$row["id_proveedor"].'">'.$row["razon_social"].'</option>';} ?>          
-               </select> 
-          </div>
-        </div>
-
-        <input type="hidden" name="estado" value="1">
-        <input type="hidden" name="id_empresa" value="<?php echo $_SESSION["id_empresa"]?>">
-        <input type="hidden" name="autor" value="<?php echo $_SESSION["nombre"]." ".$_SESSION["apellido"]; ?>">
-          <div class="col s12 center">
-            <button  name="acc" value="c" class="waves-effect black btn">Enviar</button>
-            <button class="waves-effect black btn"><a href="index.php">Cancelar</a></button>
-          </div>
-      </div>
-
-  </form> 
+          
+  </form>
   </div>
+  </div>
+  <div class="col-sm-12 col-md-5 col-lg-3 menurapido">
+  <img src="recursos/logos/logo.png" class="img-responsive" alt="Cinque Terre">
+</div>
+  </div>
+
 	
